007340a6f6310c7a38195bd0ba1180c2bbafc2ba
h0r1-m <h0r1.cnm.bio@gmail.com>
2026-02-23 22:09:06 +0000

    3章課題2まで

---
+++ exercises/03_control/01/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int[] numbers = Enumerable.Range(1, 100).ToArray();
+
+foreach (var v in numbers)
+{
+    if (v%3 == 0 && v%5 == 0)
+    {
+        Console.WriteLine("FizzBuzz");
+    }
+
+    else if (v%3 == 0)
+    {
+        Console.WriteLine("Fizz");
+    }
+
+    else if (v%5 == 0)
+    {
+        Console.WriteLine("Buzz");
+    }
+
+    else
+    {
+        Console.WriteLine(v);
+    }
+}
---
+++ exercises/03_control/02/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int[] numbers = Enumerable.Range(1, 20).ToArray();
+
+List<int> evennumbers = new List<int>();
+
+foreach (int number in numbers)
+{
+    if (number%2 == 0)
+    {
+        evennumbers.Add(number);
+    }
+}
+
+Console.WriteLine("even numbers list");
+//Console.WriteLine(string.Join(evennumbers));
+
+foreach (int evennumber in evennumbers)
+{
+    Console.WriteLine(evennumber);
+}
