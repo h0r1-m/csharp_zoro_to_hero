995740a3b193c09754d3f6f2aea0e1380b56d298
h0r1-m <h0r1.cnm.bio@gmail.com>
2026-02-27 12:52:56 +0000

    例外挑戦中

--- exercises/03_control/03/Program.cs
+++ exercises/03_control/03/Program.cs
@@ -1,3 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
+int [] n = Enumerable.Range(1, 4).ToArray();
+foreach (int i in n){
+    switch (i)
+{
+    case 1:
+        Console.WriteLine("One");
+        break;
+    case 2:
+        Console.WriteLine("Two");
+        break;
+    default:
+        Console.WriteLine("Other");
+        break;
+}}
