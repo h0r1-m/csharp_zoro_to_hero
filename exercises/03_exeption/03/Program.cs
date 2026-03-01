f5358ed789fda63a26a1be3a8bf4bde880cc0ed6
h0r1-m <h0r1.cnm.bio@gmail.com>
2026-03-01 03:22:26 +0000

    4章挑戦中

--- exercises/03_exeption/03/Program.cs
+++ exercises/03_exeption/03/Program.cs
@@ -1,4 +1,60 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-public class 
+public class InsufficientFundsException : Exception
+{
+    public InsufficientFundsException(string message) : base(message)
+    { }
+}
+
+class Program
+{
+    static int balance = 1000; //残高
+
+    static void Main()
+    {
+        try
+        {
+            Console.Write("出金額を入力してください: ");
+            int amount = int.Parse(Console.ReadLine() ?? "0");
+
+            Withdraw(amount);
+            Console.WriteLine("出金完了");
+        }
+        // ビジネス例外への対処
+        // メソッド内で条件を定義している
+        catch (InsufficientFundsException ex)
+        {
+            Console.WriteLine($"【業務エラー】 {ex.Message}");
+        }
+        // システム的な不備への対処
+        // 引数が無効→負の値を入力した場合（int.Parse、メソッド内の引数で引っかかる）
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine($"【システム利用エラー】 {ex.Message}");
+        }
+        // 文字列などはここ
+        catch (FormatException ex)
+        {
+            Console.WriteLine("【システム利用エラー】不正な入力です。" + ex.Message);
+        }
+        // その他、予期せぬエラー（バグ等）への対処
+        catch (Exception ex)
+        {
+            Console.WriteLine("【致命的エラー】深刻なエラーが発生しました。" + ex.Message);
+        }
+    }
+
+    static void Withdraw(int amount)
+    {
+        if (amount < 0)
+        {
+            throw new ArgumentException("不正な操作です。");
+        }
+        if (amount > balance)
+        {
+        throw new InsufficientFundsException("残高が足りません。");
+        }
+        balance -= amount;
+    }
+}
---
+++ exercises/03_try_catch/03/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+try
+{
+    //ユーザー名の表示
+    Console.WriteLine("ユーザー名の入力");
+    string? userName = Console.ReadLine() ?? "";
+    userName = userName.Length == 0 ? "ゲスト" : userName;
+    Console.WriteLine(userName + "さん");
+
+    //年齢のinput
+}
+catch (FormatException)
+{
+    //ユーザー名のエラー
+    Console.WriteLine("エラー： ユーザー名を入力してください。");
+}
+catch (Exception ex)
+{
+    //その他予期せぬエラー全般
+    Console.WriteLine($"予期せぬエラーが発生しました: {ex.Message}");
+}
+
+
+try
+{
+    //年齢
+    Console.WriteLine("年齢の入力");
+    int age = int.Parse(Console.ReadLine() ?? "0");
+    
+    if (age < 0)
+    {
+        throw new ArgumentOutOfRangeException();
+    }    
+
+    Console.WriteLine(age + "歳");
+}
+catch (FormatException)
+{
+    Console.WriteLine("数字を入力してください");
+}
+catch (ArgumentOutOfRangeException)
+{
+    Console.WriteLine("年齢が正しくありません");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"予期せぬエラーが発生しました: {ex.Message}");
+}
+finally
+{
+    //終了
+    Console.WriteLine("処理を終了します。");
+}
