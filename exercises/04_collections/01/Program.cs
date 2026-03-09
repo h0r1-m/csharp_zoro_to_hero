bf81988bfb698b1732a15d8c127380dcd0295b0a
Hori Maiko(堀 舞子) <maiko.hori@cct-inc.co.jp>
2026-03-09 22:00:41 +0900

    4-3挑戦中

--- exercises/04_collections/01/Program.cs
+++ exercises/04_collections/01/Program.cs
@@ -1,2 +1,62 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] a = new int[3];
+
+        // 自分で書いたコード
+        //        try
+        //        {
+        //            Console.WriteLine("1つ目の数字>>");
+        //            a[0] = int.Parse(Console.ReadLine());
+        //            Console.WriteLine("2つ目の数字>>");
+        //            a[1] = int.Parse(Console.ReadLine());
+        //            Console.WriteLine("3つ目の数字>>");
+        //            a[2] = int.Parse(Console.ReadLine());
+        //        }
+        //        catch (FormatException)
+        //        {
+        //            Console.WriteLine("数字を入力してください");
+        //        }
+        //        catch(Exception ex)
+        //        {
+        //            Console.WriteLine(ex.Message);
+        //        }
+
+
+        // chatGPTに添削してもらったコード
+
+        for (int i = 0; i < a.Length; i++)
+        {
+            while (true)
+            {
+                Console.WriteLine($"{i + 1}つ目の数字>>");
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out int result))
+                {
+                    a[i] = result;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("数字を入力してください");
+                }
+            }
+        }
+        // ここまで
+
+        Console.WriteLine("合計:");
+        var sum = a.Sum();
+        Console.WriteLine(sum);
+
+        Console.WriteLine("平均:");
+        var ave = a.Average();
+        Console.WriteLine(ave);
+    }
+}
+
---
+++ exercises/04_collections/02/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        var list = new List<int>();
+
+        while (true)
+        {
+            Console.Write("整数を入力してください（終了するにはexitを入力）:");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out int result))
+            {
+                list.Add(result);
+            }
+            else if(input.ToLower() == "exit")
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("有効な整数を入力してください");
+            }
+        }
+
+        var evenNumbers = list.Where(x => x % 2 == 0).ToList();
+
+        Console.WriteLine("偶数:");
+        foreach (var num in evenNumbers)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
