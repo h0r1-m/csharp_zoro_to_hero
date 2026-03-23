612cb5e151e36592bc2393982b418001741912fd
Hori Maiko(堀 舞子) <maiko.hori@cct-inc.co.jp>
2026-03-23 21:50:17 +0900

    ファイル整理

---
+++ exercises/IPOexercise/App/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using TsvFileFinder;
+using TsvLoader;
+using InvoiceEntity;
+
+
+//  ここで実行
--- exercises/IPOexercise/TsvLoader/TsvLoader.cs
+++ exercises/IPOexercise/TsvLoader/TsvLoader.cs
@@ -3,16 +3,17 @@
 using System;
 using System.IO;
 using System.Security.Cryptography.X509Certificates;
+using System.Collections.Generic;
 using System.Text;
 using System.Linq;
 
 public class TsvLoader
 {
-    public static IEnumerable<(string FilePath, string[] row)> Load(List<string[]> filePath)
+    public static IEnumerable<(string FilePath, string[] row)> Load(List<string> filePath)
     {
         Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 
-        foreach (string[] file in filePath)
+        foreach (string file in filePath)
         {
             using var sr = new StreamReader(file, Encoding.GetEncoding("shift_jis"), detectEncodingFromByteOrderMarks: true);
             string? line;
